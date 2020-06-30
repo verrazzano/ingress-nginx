b07335299372cdd7d846119749877406da736d6b
Michael Gianatassio <michael.gianatassio@oracle.com>
2020-06-30 10:31:57 -0400

    Stop using alphine image for custom-error-pages docker image

--- build-images.sh
+++ build-images.sh
@@ -14,3 +14,9 @@ mkdir -p images/nginx/rootfs/stage-licenses
 cp LICENSE README.md THIRD_PARTY_LICENSES.txt images/nginx/rootfs/stage-licenses
 docker build --rm=true --tag=${DOCKER_REPO}/${DOCKER_NAMESPACE}/ingress-nginx:${DOCKER_TAG} -f images/nginx/rootfs/Dockerfile.ol8-slim images/nginx/rootfs/
 rm -fr images/nginx/rootfs/stage-licenses
+
+
+mkdir -p images/custom-error-pages/rootfs/stage-licenses
+cp LICENSE README.md THIRD_PARTY_LICENSES.txt images/custom-error-pages/rootfs/stage-licenses
+make build container -e BASEIMAGE=${DOCKER_REPO}/${DOCKER_NAMESPACE}/ingress-nginx:${DOCKER_TAG} -e TAG=${DOCKER_TAG} -e REGISTRY=${DOCKER_REPO} -C images/custom-error-pages/
+rm -fr images/custom-error-pages/rootfs/stage-licenses
